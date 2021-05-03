acddb215959ab688173a923d5672672dfc0ea247
Hurrific <83616837+Hurrific@users.noreply.github.com>
2021-05-03 23:25:45 +0100

    Initial commit

---
+++ menu.py
@@ -0,0 +1,45 @@
+  _    _                 _  __ _      
+ | |  | |               (_)/ _(_)                    
+ | |__| |_   _ _ __ _ __ _| |_ _  ___             Hurrific Multitool V1 | Made by: ~ Hurriic
+ |  __  | | | | '__| '__| |  _| |/ __|                                             ~ Hurrific#7181
+ | |  | | |_| | |  | |  | | | | | (__ 
+ |_|  |_|\__,_|_|  |_|  |_|_| |_|\___|
+                                      
+  
+ 
+                                                                                                                                                                                                                                                                                                                                                                                                           
+                                          .""--..__
+                     _                     []       ``-.._                   1. Pinger                                    16. Random Ip Generator
+                  .'` `'.                  ||__           `-._               
+                 /    ,-.\                 ||_ ```---..__     `-.            2. Ip Stresser                               17. Steam Account Generator
+                /    /:::\\               /|//}          ``--._  `.           
+                |    |:::||              |////}                `-. \         3. Ip Lookup                                 18. Minecraft & Spotify Generator
+                |    |:::||             //'///                    `.\        
+                |    |:::||            //  ||'                      `|       4. Port Scanner                              19. Rat Tutorial
+                /    |:::|/        _,-//\  ||                             
+               /`    |:::|`-,__,-'`  |/  \ ||                                5. Open Putty                                20. Sick Fonts (Text etc.)
+             /`  |   |'' ||           \   |||                            
+           /`    \   |   ||            |  /||                                6. Dox Tool                                  21. Phone Spoofer (SSL)
+         |`       |  |   |)            \ | ||                        
+        |          \ |   /      ,.__    \| ||                                7. USA Fake Info Generator                   22. Sms Bomber (iOS)
+        /           `         /`    `\   | ||
+       |                     /        \  / ||                                8. 30+ Phishing Sites                        23. Ip-Logger
+       |                     |        | /  ||
+       /         /           |        `(   ||                                9. Extreme Doxing Tool                       24. Botting Panel (All Socials)
+      /          .           /             ||
+     |            \          |             ||                                10. Rat/Virus Creator (Github)               25. Instagram ID Searcher
+    /             |          /             ||               
+   |\            /          |              ||                                11. Keyword Generator Tool                   26. Fortnite Aimbot/Wallhack
+   \/`-._       |           /              ||
+    //   `.    /`           |              ||                                12. Vpn/Ovh                                  27. EXIT
+   //`.    `. |             \              ||
+  ///\ `-._  )/             |              ||                                13. Open Cyberhub (Hacking Tools And More)   ~ Don't Resell This Tool ~
+ //// )   .(/               |              ||
+ ||||   ,'` )               /              //                                14. Open Vedbex (USEFUL TOOLS)               ~ Don't credit it to your own ~
+ ||||  /                    /             || 
+ `\\` /`                    |             //                                 15. Ascii Art                                ~ Enjoy! :) ~
+     |`                     \            ||  
+    /                        |           //  
+  /`                          \         //   
+/`                            |        ||    
+`-.___,-.      .-.        ___,'        (/    
